653e6e4b97ccce9c0eff9fa34383e15b32e92516
Richard Welbert <richardwelbert@gmail.com>
2020-03-12 11:15:48 +0000

    users

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ cors = require("cors"),
 http = require("http"),
 express = require("express"),
 bodyParser = require("body-parser");
+mongoose = require( 'mongoose');
 
 var app = express();
 var port = 3000;
+var userCtrl = require('./user-controller');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(require('./routes'));
+
+app.get('/users', userCtrl.createUser);
 
 app.get('/hello/:foo/:bar', function (req, res) {
     res.json({result: "Hello CCT!", data: [
@@ -25,4 +29,14 @@ app.post('/hello', function(req,res){
 
 app.listen(port, function(err){
     console.log("Listening on port:" + port);
+});
+
+mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
 });
