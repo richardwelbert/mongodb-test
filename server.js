0ad59eb7f5bb4c1d125288dc0d3d641ffba4bb05
Richard Welbert <richardwelbert@gmail.com>
2020-03-26 14:10:47 +0000

    added CRUD functionality

--- server.js
+++ server.js
@@ -7,32 +7,17 @@ mongoose = require( 'mongoose');
 
 var app = express();
 var port = 3000;
-var userCtrl = require('./user-controller');
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(require('./routes'));
-
-app.get('/users', userCtrl.createUser);
-
-app.get('/hello/:foo/:bar', function (req, res) {
-    res.json({result: "Hello CCT!", data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});
-
-app.post('/hello', function(req,res){
-    res.json({result: "The POST request was sent", data: req.body});
-
-});
 
 app.listen(port, function(err){
     console.log("Listening on port:" + port);
 });
 
 mongoose.connect('mongodb://localhost/test');
-
 mongoose.connection.on('error', (err) => { 
     console.log('Mongodb Error: ', err); 
     process.exit();
--- user-controller.js
+++ user-controller.js
@@ -10,3 +10,39 @@ exports.createUser = function(req, res) {
         res.json(user); 
 });
 };
+
+exports.getUsers = function(req, res) {
+  User.find({}, function (err, users) {
+    if (err) {
+      res.status(400).json(err); 
+    } 
+    res.json(users);
+  }); 
+};
+
+exports.getUser = function(req, res) {
+  User.findOne({_id: req.params.id}, function (err, user) {
+    if (err) {
+      res.status(400).json(err);
+    } 
+    res.json(user);
+  }); 
+};
+
+exports.updateUser = function(req, res) {
+  User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true},function (err, user) {
+    if (err) {
+      res.status(400).json(err);
+    } 
+    res.json(user);
+  }); 
+};
+
+exports.deleteUser = function(req, res) {
+  User.findByIdAndRemove(req.params.id, function (err, user) {
+    if (err) {
+      res.status(400).json(err);
+    } 
+    res.json(user);
+  }); 
+};
